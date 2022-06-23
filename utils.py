7a0af946da083b7dd7efbbdedcff020a150ce016
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2022-06-23 02:34:04 +0000

    last commit on replit

--- utils.py
+++ utils.py
@@ -1,4 +1,6 @@
 from functools import wraps
+import json
+from discord.ext import tasks
 
 def guild_only(cmd):
     @wraps(cmd)
@@ -9,8 +11,24 @@ def guild_only(cmd):
         await cmd(me, ctx, *args, **kwargs)
     return wrapper
 
+with open("configure_bot/configuration.json", "r") as file:
+    server_config = json.load(file)
+
 def check_config(type):
     def decorator(cmd):
         @wraps(cmd)
         async def wrapper(me, ctx, *args, **kwargs):
-            
+            global server_config
+            if str(ctx.guild.id) in server_config and not server_config[str(ctx.guild.id)]:
+                await ctx.respond("That command has been disabled in this server")
+                return
+            await cmd(me, ctx, *args, **kwargs)
+        return wrapper
+    return decorator
+
+@tasks.loop(minutes=5)
+async def save(self):
+    with open("configure_bot/levels.json", "w") as file:
+        json.dump(self.levels, file, sort_keys=True, indent=4)
+    print("save (config)")
+save.start() 
