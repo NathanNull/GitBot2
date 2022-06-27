a16274703f7f550b50747c4e2b96ba4bab2497d2
Brady5YT <64619026+Brady5YT@users.noreply.github.com>
2022-06-26 17:19:19 -0700

    added a command to configuration_bot for mod_bot

--- cogs/configuration_bot.py
+++ cogs/configuration_bot.py
@@ -10,5 +10,15 @@ class Configuration(commands.Cog):
         with open("configure_bot/configuration.json", "r") as file:
             self.configuration = json.load(file)
             print(list(self.configuration.keys()))
-def setup(bot):  # this is called by Pycord to setup the cog
+
+    @discord.slash_command()
+    #@guild_only
+    async def config_moderation(self, ctx, *, moderation: discord.Option(str,choices=[
+        discord.OptionChoice("Yes Moderation", "True"),
+        discord.OptionChoice("No Moderation", "False")])):
+        if moderation == True:
+            ctx.respond("Moderation is now enabled")
+        elif moderation == False:
+            ctx.respond("Moderation is now disabled")
+def setup(bot):
     bot.add_cog(Configuration(bot))
