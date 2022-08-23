f7b50936f19d1ebfbdd3a6924b8533a691942766
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2022-08-23 12:53:17 -0700

    me when the

--- encryption.py
+++ encryption.py
@@ -12,3 +12,8 @@ def decrypt(encrypted:str, key:str) -> str:
     key = bytes(key,"utf-8")
     token_bytes = Fernet(key).decrypt(bytes(encrypted,"utf-8"))
     return str(token_bytes)[2:-1]
+
+if __name__ == "__main__":
+    k, e = encrypt(input("Enter token to encrypt: "))
+    print(f"Key: {k}")
+    print(f"Encrypted: {e}")
