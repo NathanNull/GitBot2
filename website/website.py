a1e1df9bce8db53a492f0b5edab5f8d2f967fa37
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2022-10-30 15:02:59 -0700

    aaaaand wrong url too apparently. rrgg

--- website/website.py
+++ website/website.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 if is_prod:
     discord_login = \
         r"https://discord.com/api/oauth2/authorize?client_id=985641530356273182&"+\
-        r"redirect_uri=https%3A%2F%2Fnullofspace.pythonanywhere.com%2Fserverlist"+\
+        r"redirect_uri=http%3A%2F%2Fnullofspace.pythonanywhere.com%2Fserverlist"+\
         r"&response_type=token&scope=identify%20guilds%20guilds.members.read%20guilds.join"
     
     invite_link = \
