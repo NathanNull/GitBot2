d5fbd4ea9295ee6667f6bba8591826e8b7710309
Brady5YT <64619026+Brady5YT@users.noreply.github.com>
2022-12-05 10:21:04 -0800

    testing something

--- bot/utils.py
+++ bot/utils.py
@@ -49,7 +49,7 @@ class NotifDetector(Cog):
             cog.configuration[gid]["level"] = info["info"]["level"]
             cog.configuration[gid]["moderation"] = info["info"]["moderation"]
             cog.configuration[gid]["reaction_roles"] = info["info"]["reaction_roles"]
-            asyncio.run(audit.botupdate(gid))
+            audit.botupdate(gid)
             asyncio.run(cog.save())
         elif info["type"] == 'auditchannel':
             cog = self.bot.get_cog("AuditLogging")
