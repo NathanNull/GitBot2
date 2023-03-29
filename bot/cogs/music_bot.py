be150e3216dc92967f1003bfb8f373441f0b39ea
Brady5YT <64619026+Brady5YT@users.noreply.github.com>
2023-03-28 21:45:34 -0700

    testing

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -87,7 +87,7 @@ class Music(pcs.ServerCog):
     async def raw_play(self, v_info, url, vc: discord.VoiceClient, ctx):
         print(vc)
         self.audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS), self.vol)
-        vc.play(self.audio, after=lambda e: self.bot.loop.create_task(self.when_done(ctx, vc))print(e))
+        vc.play(self.audio, after=lambda e: self.bot.loop.create_task(self.when_done(ctx, vc)), print(e))
         await music_embeds.send_song_embed(v_info, self.queue, vc, ctx, self)
     
     async def leave_if_inactive(self, vc: discord.VoiceClient):
