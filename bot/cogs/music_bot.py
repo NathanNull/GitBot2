9fdc27c95594bf071406d4fbca17ab120b0c9fba
Brady5YT <64619026+Brady5YT@users.noreply.github.com>
2023-03-29 14:49:14 -0700

    testing

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -30,7 +30,7 @@ class Music(pcs.ServerCog):
 
         # vc:discord.VoiceClient = None
         # print(vc)
-        '''if get(self.bot.voice_clients, guild=self.guild) is not None:
+        if get(self.bot.voice_clients, guild=self.guild) is not None:
             # Bot is in VC in the guild that this command was run in
             vc = get(self.bot.voice_clients, guild=self.guild)
             print(self.guild)
@@ -43,12 +43,12 @@ class Music(pcs.ServerCog):
             pass
         else:
             await ctx.respond("Neither of us are in a voice channel.")
-            return'''
-        try:
+            return
+        '''try:
             vc = await ctx.author.voice.channel.connect()
         except:
             ctx.send('already in vc')
-            vc = get(self.bot.voice_clients, guild=self.guild)
+            vc = get(self.bot.voice_clients, guild=self.guild)'''
         print('test after join if statement')
         v_info, url = self.search(query)
 
