7941465a5cff2cd8508e41dbe450deb751472acc
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2023-03-29 20:04:47 -0700

    ok cool not broken any more :)

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -29,32 +29,21 @@ class Music(pcs.ServerCog):
         await ctx.defer()
 
         vc:discord.VoiceClient = None
-        print(vc)
         if get(self.bot.voice_clients, guild=self.guild) is not None:
             # Bot is in VC in the guild that this command was run in
             vc = get(self.bot.voice_clients, guild=self.guild)
-            print(self.guild)
-            print(self.bot.voice_clients)
-            print(discord.VoiceClient)
-            print(vc.session_id)
-            print(vc.token)
-            print(vc.endpoint)
-            print(vc.channel)
-            print(vc.is_connected())
         elif ctx.author.voice != None:
             # Bot isn't in a vc, but the command's user is, so join that one
-            print('test before bot joins vc')
-            print(ctx.author.voice)
             vc = await ctx.author.voice.channel.connect()
-            pass
         else:
             await ctx.respond("Neither of us are in a voice channel.")
             return
         '''try:
-            print(ctx.author.voice.channel)
+            prijnt(ctx.author.voice.channel)
             vc = await ctx.author.voice.channel.connect()
         except:
             await ctx.send('already in vc')'''
+        
         v_info, url = self.search(query)
 
         if vc.is_playing():
@@ -65,7 +54,6 @@ class Music(pcs.ServerCog):
             if self.leave_timer != None:
                 self.leave_timer.cancel()
                 self.leave_timer = None
-            print(vc)
             await self.raw_play(v_info, url, vc, ctx)
     
     def adjust_volume(self, change):
@@ -97,7 +85,6 @@ class Music(pcs.ServerCog):
             self.leave_timer = self.bot.loop.create_task(self.leave_if_inactive(vc))
     
     async def raw_play(self, v_info, url, vc: discord.VoiceClient, ctx):
-        print(vc)
         self.audio = discord.PCMVolumeTransformer(discord.FFmpegPCMAudio(url, **FFMPEG_OPTIONS), self.vol)
         vc.play(self.audio, after=lambda e: self.bot.loop.create_task(self.when_done(ctx, vc)))
         await music_embeds.send_song_embed(v_info, self.queue, vc, ctx, self)
@@ -109,15 +96,12 @@ class Music(pcs.ServerCog):
     def search(self, query:str) -> tuple[dict, str]:
         with yt_dlp.YoutubeDL({'format': 'm4a/bestaudio/best', 'noplaylist': 'True'}) as ydl:
             try:
-                print('test')
                 requests.get(query)
             except:
-                print('test except')
                 info = ydl.extract_info(
                     f"ytsearch:{query}", download=False
                 )['entries'][0]
             else:
-                print('test else')
                 info = ydl.extract_info(query, download=False)
         return (info, info['url'])
 
