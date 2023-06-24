8aba08e1fbb1b9a3f09374ee4a3b298fed3b6b16
Brady5YT <64619026+Brady5YT@users.noreply.github.com>
2023-06-23 17:12:39 -0700

    fixed maybe prob not tho

--- bot/cogs/mod_bot.py
+++ bot/cogs/mod_bot.py
@@ -74,7 +74,7 @@ class Mod(commands.Cog):
         if str(user.id) not in self.warns[gid]:
             self.warns[gid][str(user.id)] = 0
             await self.save()
-        if str(user.id) >= 0 in self.warns[gid]:
+        if self.warns[gid][str(user.id)] >= 0:
             await ctx.respond(f'<@{user.id}> does not have any warns')
         else:
             self.warns[gid][str(user.id)] -= 1
