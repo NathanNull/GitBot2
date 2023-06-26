147c8627a05374ad4cc646cc9977fdecc8a98f7b
Brady5YT <64619026+Brady5YT@users.noreply.github.com>
2023-06-26 10:21:23 -0700

    tred

--- bot/cogs/level_bot.py
+++ bot/cogs/level_bot.py
@@ -101,7 +101,7 @@ class Level(commands.Cog):
         if gid not in self.levelroles:
             self.levelroles[gid] = {}
             print(self.levelroles)
-            await self.bot.Guild.fetch_roles()
+            #await self.bot.Guild.fetch_roles()
             await ctx.respond('testing things')
 
 
