84e778c3d409f0ede5f406ef6f595c32083fb55c
Nathan Strong <87103168+NathanNull@users.noreply.github.com>
2023-09-01 18:58:16 -0700

    fixed stuff
    Co-authored-by: Brady5YT <Brady5YT@users.noreply.github.com>

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -103,6 +103,9 @@ class Music(pcs.ServerCog):
                 )['entries'][0]
             else:
                 info = ydl.extract_info(query, download=False)
+        print(info['url'])
+        with open('test.json', 'w') as file:
+            json.dump(info, file)
         return (info, info['url'])
 
 def setup(bot):
