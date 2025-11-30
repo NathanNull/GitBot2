78e5ebd9bd759ee6eedf91088b2c5089dd4beb50
Nathan Strong <nathanstrong777@gmail.com>
2025-11-29 20:40:20 -0800

    updated updared runner file

--- run.py
+++ run.py
@@ -4,10 +4,13 @@ from sys import platform
 
 # requires that you have a venv set up, so
 # the script knows where to run python files from
-bot = [".venv/Scripts/python.exe"\
+bot = [".venv/Scripts/python.exe"
+       if platform not in ["linux", "linux2"]
+       else "python", "bot/main.py"]
+website = ["flask", "--app=website/website", "run"]\
     if platform not in ["linux", "linux2"]\
-    else "python", "bot/main.py"]
-website = "flask --app=website/website run" if platform not in ["linux", "linux2"] else "sudo ~/SurfBot/.venv/bin/flask --app=website/website.py run -h 0.0.0.0 -p 80"
+    else ["sudo", "~/SurfBot/.venv/bin/flask", "--app=website/website.py", "run", "-h", "0.0.0.0", "-p", "80"]
+
 
 def run_file(input_):
     cmd, prefix = input_
@@ -15,15 +18,20 @@ def run_file(input_):
     print(f"{prefix:>7}Started.")
     while True:
         # Get line
-        try: line = proc.stdout.readline()
+        try:
+            line = proc.stdout.readline()
 
         # Check end cases
-        except KeyboardInterrupt: break
-        if not line: break
-        if proc.returncode is not None: break
+        except KeyboardInterrupt:
+            break
+        if not line:
+            break
+        if proc.returncode is not None:
+            break
 
         # Log stdout with prefix
         print(f"{prefix:>7}{line.decode('UTF-8')}", end="")
+
 
 def main():
     proc_names = [
@@ -35,6 +43,7 @@ def main():
             pool.map(run_file, proc_names)
     except KeyboardInterrupt:
         print("Done!")
+
 
 if __name__ == "__main__":
     main()
