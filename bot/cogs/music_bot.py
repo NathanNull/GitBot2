9293cbf99fe2873671f3c0dea70742b61a7bea70
Nathan Strong <nathanstrong777@gmail.com>
2025-12-07 21:48:48 -0800

    new path

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -13,7 +13,7 @@ FFMPEG_OPTIONS = {
     'options': '-vn'
 }
 
-node_path = "C:\Program Files\nodejs\node.exe" if platform not in ["linux", "linux2"] else "~/.nvm/versions/node/v24.11.1/bin/node"
+js_path = "C:\\Program Files\\nodejs\\node.exe" if platform not in ["linux", "linux2"] else "/usr/local/bin/deno"
 
 class Music(pcs.ServerCog):
     def __init__(self, *args):
@@ -107,7 +107,7 @@ class Music(pcs.ServerCog):
             'cookiefile': './cookies.txt',
             'format': 'm4a/bestaudio/best',
             'noplaylist': True,
-            # 'js_runtimes': {'node': {'path': node_path}},
+            'js_runtimes': {'deno': {'path': js_path}},
         }) as ydl:
             try:
                 r = requests.get(query, stream=True, timeout=3)
