538a012496b2b8ee69939bd3c162e0352851cf4e
Brady5YT <smithbrayden2005@gmail.com>
2026-06-18 21:04:38 -0700

    test

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -199,7 +199,7 @@ class Music(pcs.ServerCog):
 
         
         'http_headers': {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36'
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36'
         },
         
         'nocheckcertificate': True,
