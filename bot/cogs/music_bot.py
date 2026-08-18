e8dff1e1529adea51e4878a077d90932faf1c8bc
Nathan Strong <nathanstrong777@gmail.com>
2026-08-18 15:18:15 -0700

    fix att1 for ytdlp issue

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -189,12 +189,12 @@ class Music(pcs.ServerCog):
         # OR better if bot runs on same machine as browser:
         # 'cookiesfrombrowser': ('chrome',),     # or firefox, edge
         
-        #'extractor_args': {
-        #    'youtube': {
-        #        'player_client': ['ios', 'android', 'web'],   # Very important in 2026
+        'extractor_args': {
+           'youtube': {
+               'player_client': ['default', 'web_embedded'],   # Very important in 2026
                 # 'po_token': '...'   # advanced, optional
-        #    }
-        #},
+           }
+        },
         
         'nocheckcertificate': True,
         
