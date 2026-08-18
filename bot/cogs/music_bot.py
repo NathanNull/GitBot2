a2eb1b798d62d3c153f39c7179ec9463832ccc7f
Nathan Strong <nathanstrong777@gmail.com>
2026-08-18 16:38:45 -0700

    fix att11 for ytdlp

--- bot/cogs/music_bot.py
+++ bot/cogs/music_bot.py
@@ -136,7 +136,7 @@ class Music(pcs.ServerCog):
                 '-reconnect_streamed 1 '
                 '-reconnect_delay_max 5 '
                 '-seekable 0 '
-                '-4 -nostdin'
+                '-nostdin'
             ), options="-vn"), self.vol)
         vc.play(self.audio, after=lambda e: self.bot.loop.create_task(
             self.when_done(ctx, vc)))
@@ -176,6 +176,7 @@ class Music(pcs.ServerCog):
         },
         
         'nocheckcertificate': True,
+        'source_address': '0.0.0.0',
         
         #'http_headers': {
         #    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/149.0.0.0 Safari/537.36'
